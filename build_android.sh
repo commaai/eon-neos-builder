4aafbe1143775845a8ce1c3cc453f389d09ba36e
George Hotz <george@comma.ai>
2018-06-12 20:38:06 -0700

    builder for android

---
+++ build_android.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir -p ~/android/system
+cd ~/android/system
+repo init -u git@github.com:commaai/android.git
+repo sync
+
+source build/envsetup.sh
+breakfast oneplus3
+brunch oneplus3
+
