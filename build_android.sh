4a0e7541f911d502ce8a93fbb7f19a5e5c478cb4
George Hotz <george@comma.ai>
2018-06-13 15:12:37 -0700

    fix build android

--- build_android.sh
+++ build_android.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 mkdir -p mindroid/system
 cd mindroid/system
-tools/repo init -u git@github.com:commaai/android.git -b mindroid
-tools/repo sync
+../../tools/repo init -u git@github.com:commaai/android.git -b mindroid
+../../tools/repo sync
 
 source build/envsetup.sh
 breakfast oneplus3
-brunch oneplus3
+make -j bootimage recoveryimage systemimage
 
