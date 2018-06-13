ea4daff95aa416079cc12cc0d5e728f67a0d34c1
George Hotz <george@comma.ai>
2018-06-13 01:07:34 -0700

    make system

---
+++ home/.bash_profile
@@ -0,0 +1,10 @@
+export EDITOR='vim'
+export TERM='xterm-256color'
+export PS1="\u@\h:\[\e[36m\]\w\[\e[0m\]\$ "
+export PREFIX='/usr'
+export VALGRIND_LIB='/usr/lib/valgrind'
+
+# add these symlinks if you want
+export PYTHONPATH="/data/pythonpath"
+export GIT_SSH_COMMAND="ssh -i /data/gitkey"
+
--- make_system.sh
+++ make_system.sh
@@ -7,6 +7,8 @@ if [ ! -d usr ]; then
 fi
 (cd usr && git pull)
 
+FIRMWARE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 mkdir -p build
 pushd build
   mkdir -p system/mnt
@@ -14,16 +16,29 @@ pushd build
 
   # prepare system image
   pushd system
-    simg2img system.img system.img.raw
+    simg2img "$FIRMWARE_DIR"/android/system/out/target/product/oneplus3/system.img system.img.raw
     mkdir -p mnt
     sudo mount -o loop system.img.raw mnt
 
-    ../../populate_system.sh mnt/
+    # copy the usr files
+    echo "doing git copy with usr repo"
+    sudo mkdir -p mnt/comma
+    sudo cp -R "$FIRMWARE_DIR"/usr mnt/comma
 
+    echo 'replacing properties'
+    sudo sed -i 's/ro.adb.secure=1/ro.adb.secure=0/' mnt/build.prop
+    sudo sed -i 's/neos.vpn=1/neos.vpn=0/' mnt/build.prop
+
+    # copy the home files
+    sudo cp -Rv "$FIRMWARE_DIR"/home mnt/comma/home
+    sudo chmod 600 mnt/comma/home/.ssh/*
+
+    # set permissions
     sudo chown root:root mnt/build.prop
     sudo chmod 644 mnt/build.prop
     sudo chmod 600 mnt/comma/usr/etc/ssh/*
     sudo chmod 600 mnt/comma/home/.ssh/*
+    sudo chmod 600 mnt/comma/usr/etc/ssh/*
 
     if [[ $LEECO ]]; then
       echo "copying LeEco files"
