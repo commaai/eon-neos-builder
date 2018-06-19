ec5717056becfefd41cfad8c668d9256157ee923
George Hotz <george@comma.ai>
2018-06-18 22:35:42 -0700

    leeco

--- reflash_system.sh
+++ reflash_system.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./build_android.sh && ./make_system.sh && fastboot oem 4F500301 && fastboot flash system build/system.good.img && fastboot reboot
+./build_android.sh && ./make_system.sh && $(fastboot oem 4F500301; true) && fastboot flash system build/system.good.img && fastboot reboot
