49c6ce1e2f0ee44a0d87a54463e7a662464b0700
George Hotz <george@comma.ai>
2018-06-26 10:43:44 -0700

    jdk

--- build_android.sh
+++ build_android.sh
@@ -4,6 +4,8 @@ cd mindroid/system
 ../../tools/repo init -u https://github.com/commaai/android.git -b mindroid
 ../../tools/repo sync
 
+sudo apt-get install openjdk-8-jdk
+
 source build/envsetup.sh
 breakfast oneplus3
 make -j
