fa929cd7a82677b01f31f149685f7d31b3bf7725
Your Name <you@example.com>
2018-07-09 11:18:45 -0700

    add missing packages

--- build_android.sh
+++ build_android.sh
@@ -5,6 +5,7 @@ cd mindroid/system
 ../../tools/repo sync
 
 sudo apt-get install -y openjdk-8-jdk git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc unzip
+sudo apt-get install -y android-tools-fsutils abootimg
 
 source build/envsetup.sh
 breakfast oneplus3
