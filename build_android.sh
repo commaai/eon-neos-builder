8ca6b538f2238cc87aecbfa73893ebc668b8778a
George Hotz <george@comma.ai>
2018-09-21 20:56:47 -0700

    make build_android more reliable

--- build_android.sh
+++ build_android.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/bash -e
 mkdir -p mindroid/system
 cd mindroid/system
 ../../tools/repo init -u https://github.com/commaai/android.git -b mindroid
-../../tools/repo sync
+../../tools/repo sync --force-sync
 
 sudo apt-get install -y openjdk-8-jdk git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc unzip
 
