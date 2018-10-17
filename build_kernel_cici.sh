86a63eeb25fa2fe230c5a308a330e84d9189ee4a
George Hotz <george@comma.ai>
2018-10-16 17:18:16 -0700

    fix toolchain path

--- build_kernel_cici.sh
+++ build_kernel_cici.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+export CROSS_COMPILE=$PWD/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export ARCH=arm64
+
 git clone git@github.com:commaai/android_kernel_cici_msm8996.git --depth 1
 cd android_kernel_cici_msm8996
 git pull
-
-export CROSS_COMPILE=/opt/android-ndk/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/aarch64-linux-android-
-export ARCH=arm64
 
 make cici_defconfig
 make -j$(nproc --all)
