fb835a4886cbcfb82cfa3658b7b8cc74c6014b66
George Hotz <george@comma.ai>
2018-10-20 00:26:27 -0700

    dash e is a good choice

--- build_bootloader_cici.sh
+++ build_bootloader_cici.sh
@@ -5,6 +5,8 @@ cd bootloader
 git clone git://codeaurora.org/platform/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8.git --depth 1 -b LA.BR.1.1.3.c4-01000-8x16.0 android-gcc
 git clone --depth 1 git@github.com:commaai/lk_cici.git
 
+set -e
+
 # build
 cd lk_cici
 make -j4 msm8996 EMMC_BOOT=1 VERIFIED_BOOT=1 TOOLCHAIN_PREFIX=../android-gcc/bin/arm-eabi-
