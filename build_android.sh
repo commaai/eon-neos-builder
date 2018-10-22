9131248e6a94527e783445fbb28deb72d9940bce
Willem Melching <willem.melching@gmail.com>
2018-10-22 08:47:50 +0200

    add env variable to skip dependency install

--- build_android.sh
+++ build_android.sh
@@ -4,9 +4,10 @@ cd mindroid/system
 ../../tools/repo init -u https://github.com/commaai/android.git -b mindroid
 ../../tools/repo sync --force-sync
 
-sudo apt-get install -y openjdk-8-jdk git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc unzip
+if [[ -z "${SKIP_DEPS}" ]]; then
+    sudo apt-get install -y openjdk-8-jdk git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc unzip python bc android-tools-fsutils abootimg
+fi
 
 source build/envsetup.sh
 breakfast oneplus3
 make -j
-
