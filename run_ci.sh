12ef080fd9c94ac295fed470e80fe1bccacaf2c0
Willem Melching <willem.melching@gmail.com>
2018-10-24 14:13:56 -0700

    limit output to not trigger 4mb log size limit

--- run_ci.sh
+++ run_ci.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
+show_log() {
+  tail -n 1000 log.txt
+}
+
+trap "show_log" ERR
+
+{
 ./build_android.sh
 ./build_kernel_leeco.sh
 ./build_kernel_oneplus.sh
@@ -8,6 +15,9 @@ set -e
 ./make_boot.sh oneplus
 ./make_recovery.sh leeco
 ./make_recovery.sh oneplus
+} > log.txt
+
+tail -n 1000 log.txt
 
 # TODO: Figure out mounting inside docker
 # ./make_system.sh
