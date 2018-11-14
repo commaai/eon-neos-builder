65befa80c3f49cd827032259cc8d575ea3cdc2a6
Sultan Alsawaf <sultanxda@gmail.com>
2018-11-13 22:43:12 -0800

    Fix toolchain extraction
    Silence the annoying console spam, and edit the Arch-compiled GCC to work
    on Ubuntu 16.04.

--- tools/extract_toolchains.sh
+++ tools/extract_toolchains.sh
@@ -8,28 +8,37 @@
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 
+cd $DIR
+
 LATEST_FSF_BINUTILS=aarch64-linux-gnu-binutils
 LATEST_FSF_GCC=aarch64-linux-gnu-gcc
 GOOGLE_GCC_4_9=aarch64-linux-android-4.9
 GOOGLE_GCC_4_8_32BIT=arm-eabi-4.8
 
-LATEST_FSF_BINUTILS_TARBALL=$(find $DIR -name $LATEST_FSF_BINUTILS*.xz)
-LATEST_FSF_GCC_TARBALL=$(find $DIR -name $LATEST_FSF_GCC*.xz)
-GOOGLE_GCC_4_9_TARBALL=$(find $DIR -name $GOOGLE_GCC_4_9*.gz)
-GOOGLE_GCC_4_8_32BIT_TARBALL=$(find $DIR -name $GOOGLE_GCC_4_8_32BIT*.gz)
+LATEST_FSF_BINUTILS_TARBALL=$(find . -name $LATEST_FSF_BINUTILS*.xz)
+LATEST_FSF_GCC_TARBALL=$(find . -name $LATEST_FSF_GCC*.xz)
+GOOGLE_GCC_4_9_TARBALL=$(find . -name $GOOGLE_GCC_4_9*.gz)
+GOOGLE_GCC_4_8_32BIT_TARBALL=$(find . -name $GOOGLE_GCC_4_8_32BIT*.gz)
 
-if [ ! -d $DIR/$LATEST_FSF_GCC ]; then
-  mkdir $DIR/$LATEST_FSF_GCC
-  tar -xJf $LATEST_FSF_GCC_TARBALL -C $DIR/$LATEST_FSF_GCC
-  tar -xJf $LATEST_FSF_BINUTILS_TARBALL -C $DIR/$LATEST_FSF_GCC
+if [ ! -d $LATEST_FSF_GCC ]; then
+  mkdir $LATEST_FSF_GCC
+  tar -xJf $LATEST_FSF_GCC_TARBALL -C $LATEST_FSF_GCC &>/dev/null
+  tar -xJf $LATEST_FSF_BINUTILS_TARBALL -C $LATEST_FSF_GCC &>/dev/null
+
+  if [ ! -e /usr/lib/libmpfr.so.6 ]; then
+    echo "libmpfr.so.6 not found, editing GCC to use libmpfr.so.4 instead"
+    find $LATEST_FSF_GCC -type f -executable -exec sed -i "s/libmpfr.so.6/libmpfr.so.4/g" {} \;
+  fi
 fi
 
-if [ ! -d $DIR/$GOOGLE_GCC_4_9 ]; then
-  mkdir $DIR/$GOOGLE_GCC_4_9
-  tar -xzf $GOOGLE_GCC_4_9_TARBALL -C $DIR/$GOOGLE_GCC_4_9
+if [ ! -d $GOOGLE_GCC_4_9 ]; then
+  mkdir $GOOGLE_GCC_4_9
+  tar -xzf $GOOGLE_GCC_4_9_TARBALL -C $GOOGLE_GCC_4_9 &>/dev/null
 fi
 
-if [ ! -d $DIR/$GOOGLE_GCC_4_8_32BIT ]; then
-  mkdir $DIR/$GOOGLE_GCC_4_8_32BIT
-  tar -xzf $GOOGLE_GCC_4_8_32BIT_TARBALL -C $DIR/$GOOGLE_GCC_4_8_32BIT
+if [ ! -d $GOOGLE_GCC_4_8_32BIT ]; then
+  mkdir $GOOGLE_GCC_4_8_32BIT
+  tar -xzf $GOOGLE_GCC_4_8_32BIT_TARBALL -C $GOOGLE_GCC_4_8_32BIT &>/dev/null
 fi
+
+echo "Toolchains extracted!"
