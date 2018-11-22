6a4a1ade903f79659ce8e5ad12f2a23c59a6327c
Andy Haden <andy@comma.ai>
2018-11-21 17:22:56 -0800

    write update json in prepare_ota

--- prepare_ota.sh
+++ prepare_ota.sh
@@ -3,6 +3,7 @@ mkdir -p eon-neos
 
 echo "copying files"
 RECOVERY_HASH=$(sha256sum build/recoverynew.img | cut -c 1-64)
+RECOVERY_LEN=$(wc -c build/recoverynew.img | awk '{print $1}')
 cp -v build/recoverynew.img eon-neos/recovery-$RECOVERY_HASH.img
 rm -f eon-neos/recovery.img
 ln -s recovery-$RECOVERY_HASH.img eon-neos/recovery.img
@@ -18,3 +19,13 @@ mv eon-neos/ota-signed.zip eon-neos/ota-signed-$OTA_HASH.zip
 rm -f eon-neos/ota-signed-latest.zip
 ln -s ota-signed-$OTA_HASH.zip eon-neos/ota-signed-latest.zip
 
+NEOS_UPDATE_URL=${NEOS_UPDATE_URL:-https://commadist.azureedge.net/neosupdate}
+tee eon-neos/update.json > /dev/null <<EOF
+{
+  "ota_url": "$NEOS_UPDATE_URL/ota-signed-$OTA_HASH.zip",
+  "ota_hash": "$OTA_HASH",
+  "recovery_url": "$NEOS_UPDATE_URL/recovery-$RECOVERY_HASH.img",
+  "recovery_len": $RECOVERY_LEN,
+  "recovery_hash": "$RECOVERY_HASH"
+}
+EOF 
