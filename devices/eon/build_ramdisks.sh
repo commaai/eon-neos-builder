51148d87f35106e1e8ab691ddfb19662dabe14ba
Andy Haden <andyh2@me.com>
2018-11-29 20:33:19 -0800

    fix ramdisk path

--- devices/eon/build_ramdisks.sh
+++ devices/eon/build_ramdisks.sh
@@ -4,5 +4,5 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 cd "$DIR"
 
 ./build_ramdisk_boot.sh
-cp "mindroid/system/out/target/product/oneplus3/ramdisk-recovery.img" ramdisk-recovery.img
+cp "mindroid/system/out/target/product/oneplus3/ramdisk-recovery.img" ramdisk-recovery.gz
 
