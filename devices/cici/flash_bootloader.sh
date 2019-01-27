91cd72933124e5ca13f6827e5c8b1690b9c6fd3b
Bruce Wayne <eddiemsamuels@gmail.com>
2019-01-26 18:26:39 -0800

    fix up bootloaders and bootloader flasher

--- devices/cici/flash_bootloader.sh
+++ devices/cici/flash_bootloader.sh
@@ -1,11 +1,23 @@
 #!/bin/bash -e
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+BOARD="$1"
+
+if [ -z "$BOARD" ]; then
+  echo "default to inforce"
+  BOARD="normsom"
+else
+  if [ -d "bootloader/$BOARD" ]; then
+    echo "flashing $BOARD"
+  else
+    echo "no $BOARD support"
+  fi
+fi
 
 cd $DIR
 
 cd bootloader/qdl
 make -j$(nproc --all)
 
-cd ../microsom
+cd ../$BOARD
 sudo ../qdl/qdl prog_ufs_firehose_8996_ddr.elf rawprogram.xml patch.xml
