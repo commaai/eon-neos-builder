8a117ae6346ef3f48817674bab953862c860dcba
Sultan Alsawaf <sultan@kerneltoast.com>
2019-03-25 16:46:27 -0700

    eon: Make flash scripts flash to the correct partitions

--- devices/eon/flash_recovery.sh
+++ devices/eon/flash_recovery.sh
@@ -4,4 +4,4 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 OUT=$DIR/out
 
 fastboot oem 4F500301 1>/dev/null 2>&1
-fastboot flash boot $OUT/recovery.img
+fastboot flash recovery $OUT/recovery.img
--- devices/eon/flash_system.sh
+++ devices/eon/flash_system.sh
@@ -4,4 +4,4 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 OUT=$DIR/out
 
 fastboot oem 4F500301 1>/dev/null 2>&1
-fastboot flash boot $OUT/system.img
+fastboot flash system $OUT/system.img
