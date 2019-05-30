ac665f4b76c382fb09e48bdabf4af6a613d24f68
Robbe Derks <robbe.derks@gmail.com>
2019-05-30 08:53:43 -0700

    Renamed cici to tici

--- devices/tici/build_bootloader.sh
+++ devices/tici/build_bootloader.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-if [ ! -d edk2_cici ]; then
-  git clone git@github.com:commaai/edk2_cici.git
+if [ ! -d edk2_tici ]; then
+  git clone git@github.com:commaai/edk2_tici.git
 fi
 
-cd edk2_cici
+cd edk2_tici
 
 # TODO: get builds outside the android tree working
 make
