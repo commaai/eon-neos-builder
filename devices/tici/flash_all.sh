c0f1048371008f903623e0ea1aaf7f5048eb4c23
George Hotz <george@comma.ai>
2019-08-06 20:31:52 -0700

    update build and flash

--- devices/tici/flash_all.sh
+++ devices/tici/flash_all.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-OUTPATH=android/out/target/product/sdm845
+#OUTPATH=android/out/target/product/sdm845
+OUTPATH=out
 
 fastboot flash boot $OUTPATH/boot.img
 fastboot flash dtbo $OUTPATH/dtbo.img
-fastboot flash system $OUTPATH/system.img
+fastboot flash system $OUTPATH/system.simg
+fastboot flash vendor $OUTPATH/vendor.simg
 fastboot flash vbmeta $OUTPATH/vbmeta.img --disable-verity
-fastboot flash vendor $OUTPATH/vendor.img
+fastboot format userdata
 
