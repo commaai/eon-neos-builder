7ddeca9b6b9cc72a80035dd5e103c378ec43b0ab
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2019-08-23 15:22:31 -0700

    NEOS 10.5

--- devices/eon/build_ramdisk_boot.sh
+++ devices/eon/build_ramdisk_boot.sh
@@ -22,7 +22,7 @@ pushd boot_ramdisk
   ln -s /data/data/com.termux/files/usr usr
   sudo cp -v "$DIR"/ramdisk_common/* .
   echo "10" > VERSION
-  echo "4" > REVISION
+  echo "5" > REVISION
   touch EON
 
   # repack ramdisk
