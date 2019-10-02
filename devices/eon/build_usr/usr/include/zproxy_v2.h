51a95f3b69ad2c783a0b8e23e2a8ab23d6f4be5b
Willem Melching <willem.melching@gmail.com>
2019-10-01 17:47:06 -0700

    copy ssh keys in init

--- devices/eon/build_usr/usr/include/zproxy_v2.h
+++ devices/eon/build_usr/usr/include/zproxy_v2.h
@@ -1,3 +1,62 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:630b2d19cd98817c3ec2893f398466cc55b2a26e71e88b3a05dd940a681e93da
-size 1957
+/*  =========================================================================
+    zproxy_v2 - run a steerable proxy in the background (deprecated)
+
+    Copyright (c) the Contributors as noted in the AUTHORS file.
+    This file is part of CZMQ, the high-level C binding for 0MQ:
+    http://czmq.zeromq.org.
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+    =========================================================================
+*/
+
+#ifndef __ZPROXY_V2_H_INCLUDED__
+#define __ZPROXY_V2_H_INCLUDED__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//  @interface
+
+//  Constructor
+//  Create a new zproxy object. You must create the frontend and backend
+//  sockets, configure them, and connect or bind them, before you pass them
+//  to the constructor. Do NOT use the sockets again, after passing them to
+//  this method.
+CZMQ_EXPORT zproxy_t *
+    zproxy_new (zctx_t *ctx, void *frontend, void *backend);
+
+//  Destructor
+//  Destroy a zproxy object; note this first stops the proxy.
+CZMQ_EXPORT void
+    zproxy_destroy (zproxy_t **self_p);
+
+//  Copy all proxied messages to specified endpoint; if this is NULL, any
+//  in-progress capturing will be stopped. You must already have bound the
+//  endpoint to a PULL socket.
+CZMQ_EXPORT void
+    zproxy_capture (zproxy_t *self, const char *endpoint);
+
+//  Pauses a zproxy object; a paused proxy will cease processing messages,
+//  causing them to be queued up and potentially hit the high-water mark on
+//  the frontend socket, causing messages to be dropped, or writing
+//  applications to block.
+CZMQ_EXPORT void
+    zproxy_pause (zproxy_t *self);
+
+//  Resume a zproxy object
+CZMQ_EXPORT void
+    zproxy_resume (zproxy_t *self);
+
+// Self test of this class
+CZMQ_EXPORT void
+    zproxy_v2_test (bool verbose);
+//  @end
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
