63ff3c8fc7eeda865e8101114c90019d2842077f
George Hotz <geohot@gmail.com>
2020-03-19 22:18:24 -0700

    parse strings

--- devices/tici/devcfg/load.py
+++ devices/tici/devcfg/load.py
@@ -17,27 +17,59 @@ la = ss.header['p_vaddr']
 print(hex(la))
 
 d = ss.data()
-hexdump(d[0:0x100])
+#hexdump(d)
 
-"""
-for i in range(0, len(d), 4):
+#for i in range(0, len(d), 4):
+for i in range(0, 0x100, 4):
   aa = struct.unpack("I", d[i:i+4])[0] - la
   if aa >= 0 and aa < len(d):
     ss = d[aa:aa+0x100].split(b"\x00")[0]
     print(hex(i), hex(aa), ss)
 
-"""
+def g32(i):
+  return struct.unpack("I", d[i:i+4])[0]
 
-off = struct.unpack("I", d[0x48:0x48+4])[0] - la
-cnt = 0
+def gs(i):
+  return d[i:i+0x100].split(b"\x00")[0]
+
+print("****** 0x40 table ******")
+off = g32(0x40) - la
+table_length = g32(off)
+tbl = d[off:off+table_length]
+
+s1 = tbl[g32(off+4):g32(off+8)].split(b"\x00")
+s2 = tbl[g32(off+8):g32(off+0xc)].split(b"\x00")
+
+hexdump(tbl)
+print(s1)
+print(s2)
+
+
+
+"""
+print("****** 0x48 table ******")
+# the "0x48" table
+# 0x10 entries
+off = g32(0x48) - la
 while 1:
   l,a = struct.unpack("QQ", d[off:off+0x10])
   if l == 0:
     break
-  print(hex(cnt), l,hex(a))
+  print(l,hex(a-la))
   off += 0x10
+  if l < 0x100:
+    hexdump(d[a-la:a-la+l])
 
-  hexdump(d[a-la:a-la+l])
-  cnt += 1
+# the "0x58" table
+# 0x28 length entries
+# number of entries in "0x50"
+print("****** 0x58 table ******")
+cnt = g32(0x50)
+off = g32(0x58) - la
+for i in range(cnt):
+  a = struct.unpack("Q", d[off:off+8])[0] - la
+  print(hex(a), gs(a))
+  off += 0x28
+"""
 
 
