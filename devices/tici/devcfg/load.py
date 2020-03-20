2ccd99490492419c47fbb6af022131406b5f8ae6
George Hotz <geohot@gmail.com>
2020-03-19 23:09:26 -0700

    parse with strings

--- devices/tici/devcfg/load.py
+++ devices/tici/devcfg/load.py
@@ -36,31 +36,46 @@ print("****** 0x40 table ******")
 off = g32(0x40) - la
 table_length = g32(off)
 tbl = d[off:off+table_length]
+hexdump(tbl[0:0x20])
 
-s1 = tbl[g32(off+4):g32(off+8)].split(b"\x00")
-s2 = tbl[g32(off+8):g32(off+0xc)].split(b"\x00")
+s1 = tbl[g32(off+4):g32(off+8)]
+s2 = tbl[g32(off+8):g32(off+0xc)]
 ee = tbl[g32(off+0xc):]
 
 print(hex(len(s1)), s1)
 print(hex(len(s2)), s2)
-hexdump(ee)
+#hexdump(ee)
 
-"""
+sidx = {}
+for i in range(0, len(ee), 4):
+  ret = struct.unpack("I", ee[i:i+4])[0]
+  if ret>>16 == 0x1280:
+    idx = struct.unpack("I", ee[i+4:i+8])[0]
+    ts = s1[ret&0xFFFF:(ret&0xFFFF)+0x100].split(b"\x00")[0]
+    print(ts)
+    sidx[idx] = ts
+  else:
+    print(hex(ret))
+  
+
 print("****** 0x48 table ******")
 # the "0x48" table
 # 0x10 entries
 off = g32(0x48) - la
+cnt = 0
 while 1:
   l,a = struct.unpack("QQ", d[off:off+0x10])
   if l == 0:
     break
-  print(l,hex(a-la))
+  print(hex(cnt), "" if cnt not in sidx else sidx[cnt], l,hex(a-la))
+  cnt += 1
   off += 0x10
   if l < 0x100:
     hexdump(d[a-la:a-la+l])
   else:
     print("TOO BIG TO PRINT")
 
+"""
 # the "0x58" table
 # 0x28 length entries
 # number of entries in "0x50"
