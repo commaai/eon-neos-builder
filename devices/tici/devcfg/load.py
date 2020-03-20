5c426053b28549a52fc7cec42ba0263dbd50caf5
George Hotz <geohot@gmail.com>
2020-03-19 22:00:02 -0700

    structure a bit

--- devices/tici/devcfg/load.py
+++ devices/tici/devcfg/load.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
+import sys
 from elftools.elf.elffile import ELFFile
 from hexdump import hexdump
 import struct
 
-e = ELFFile(open("devcfg.mbn", "rb"))
+e = ELFFile(open("devcfg.mbn" if len(sys.argv) == 1 else sys.argv[1], "rb"))
 
 # segment 0 = ELF header
 # segment 1 = cert
@@ -16,7 +17,7 @@ la = ss.header['p_vaddr']
 print(hex(la))
 
 d = ss.data()
-#hexdump(d)
+hexdump(d[0:0x100])
 
 """
 for i in range(0, len(d), 4):
@@ -28,13 +29,15 @@ for i in range(0, len(d), 4):
 """
 
 off = struct.unpack("I", d[0x48:0x48+4])[0] - la
+cnt = 0
 while 1:
   l,a = struct.unpack("QQ", d[off:off+0x10])
   if l == 0:
     break
-  print(l,a)
+  print(hex(cnt), l,hex(a))
   off += 0x10
 
   hexdump(d[a-la:a-la+l])
+  cnt += 1
 
 
