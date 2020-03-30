3e57882fa7a9d3159f567bf4ca2af3ab5f0b012a
Willem Melching <willem.melching@gmail.com>
2020-03-30 15:32:19 -0700

    Fix upload scripts

--- devices/eon/ota_push_prod.sh
+++ devices/eon/ota_push_prod.sh
@@ -10,7 +10,7 @@ DATA_ACCOUNT="commadist"
 DATA_CONTAINER="neosupdate"
 
 SAS_EXPIRY=$(date -u '+%Y-%m-%dT%H:%M:%SZ' -d '+1 hour')
-DATA_SAS_TOKEN=$(az storage container generate-sas --account-name $DATA_ACCOUNT --name $DATA_CONTAINER --https-only --permissions w --expiry $SAS_EXPIRY --output tsv)
+DATA_SAS_TOKEN=$(az storage container generate-sas --as-user --auth-mode login --account-name $DATA_ACCOUNT --name $DATA_CONTAINER --https-only --permissions w --expiry $SAS_EXPIRY --output tsv)
 
 azcopy cp $NEOSUPDATE/recovery-$RECOVERY_HASH.img "https://$DATA_ACCOUNT.blob.core.windows.net/$DATA_CONTAINER/recovery-$RECOVERY_HASH.img?$DATA_SAS_TOKEN"
 azcopy cp $NEOSUPDATE/ota-signed-$OTA_HASH.zip "https://$DATA_ACCOUNT.blob.core.windows.net/$DATA_CONTAINER/ota-signed-$OTA_HASH.zip?$DATA_SAS_TOKEN"
