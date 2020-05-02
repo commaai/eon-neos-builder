d92bd6eba1134762a2203bc139fe767289d21e55
Jason Young <jyoung8607@gmail.com>
2020-05-02 15:54:37 -0400

    Install Yarn for NEOS setup build

--- devices/eon/install_deps.sh
+++ devices/eon/install_deps.sh
@@ -7,12 +7,16 @@ echo "Starting dependency installs"
 source ~/.bashrc
 hash -r
 
-# Third party package repo required for git-lfs on Ubuntu 16.04
+# Third party package repos required for git-lfs and yarn on Ubuntu 16.04
 sudo apt-get install curl
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
 # Basic dependencies
-sudo apt-get install -y cpio git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc unzip python bc android-tools-fsutils git-lfs openjdk-8-jdk openjdk-8-jre android-sdk
+sudo apt update
+sudo apt install -y cpio git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc unzip python bc android-tools-fsutils git-lfs openjdk-8-jdk openjdk-8-jre android-sdk
+sudo apt install --no-install-recommends yarn
 
 # Additional setup for Android SDK environment and toolset
 if [[ ! -f "/usr/lib/android-sdk/tools/bin/sdkmanager" ]]; then
@@ -36,6 +40,10 @@ if [[ -z "$ANDROID_HOME" ]]; then
 else
   echo "ANDROID_HOME already set, skipping..."
 fi
+if [[ ! $(command -v yarn) ]]; then
+  echo "Installing Yarn (for building the NEOSSetup APK)"
+
+
 source ~/.bashrc
 hash -r
 
