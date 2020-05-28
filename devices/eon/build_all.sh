3e63ccab219bfad5bf1925bb128527e57920f1ba
Jason Young <jason@comma.ai>
2020-05-28 16:15:19 -0700

    Add an explicit Python 2 check

--- devices/eon/build_all.sh
+++ devices/eon/build_all.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -e
 
+PYTHON_MAJOR=`python -c "import platform; major, minor, patch = platform.python_version_tuple(); print(major)"`
+if [ "$PYTHON_MAJOR" != "2" ]; then
+  echo "Parts of the NEOS build still require \"python\" to be legacy Python 2!"
+  echo "You may have pyenv shims in your PATH pointing to Python 3."
+  exit
+fi
+
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 
 cd $DIR
