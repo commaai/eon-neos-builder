c4903db189f6672c6caa97b6284025ace67c3bb2
Jason Young <jason@comma.ai>
2020-05-29 19:58:44 -0700

    I don't wanna talk about it

--- devices/eon/build_usr/install.py
+++ devices/eon/build_usr/install.py
@@ -16,6 +16,48 @@ BASE_URL = 'http://termux.comma.ai/'
 
 DEFAULT_PKG = ['apt', 'bash', 'busybox', 'ca-certificates', 'command-not-found', 'dash', 'dash', 'dpkg', 'gdbm', 'gpgv', 'libandroid-support', 'libbz2', 'libc++', 'libcrypt', 'libcurl', 'libffi', 'libgcrypt', 'libgpg-error', 'liblzma', 'libnghttp2', 'libsqlite', 'libutil', 'ncurses', 'ncurses-ui-libs', 'openssl', 'python', 'readline', 'termux-am', 'termux-exec', 'termux-tools']
 
+# Python 3.8.2 is not available in binary form from the termux package repo,
+# but it can be built from source from the termux-packages collection. It
+# must be built locally in docker and copied to the path below.
+#
+# FIXME: still needs termux-elf stripping
+# FIXME: should probably be using the Android API level options/legacy android-5 termux repo to start
+# FIXME: explicitly test for the override package not being present and warn the user to look here
+#
+# Clone the termux repo and change the Python package build.sh and patch file
+# back to its Python 3.8.2 state (from the current 3.8.3 or other future state)
+#
+# Then replace the configure.patch file with the below: removes lockf(), the
+# preadv()/pwritev() family, and if_nameindex(), and undoes the termux patch
+# removing link() which is usable on our ext4 filesystems.
+#
+#
+#    diff -u -r ../cpython/configure ./configure
+#    --- ../cpython/configure	2020-05-29 17:02:33.994795843 -0700
+#    +++ ./configure	2020-05-29 17:44:19.524728185 -0700
+#    @@ -11484,12 +11484,11 @@
+#      getgrgid_r getgrnam_r \
+#      getgrouplist getgroups getlogin getloadavg getpeername getpgid getpid \
+#      getpriority getresuid getresgid getpwent getpwnam_r getpwuid_r getspnam getspent getsid getwd \
+#    - if_nameindex \
+#    - initgroups kill killpg lchown lockf linkat lstat lutimes mmap \
+#    + initgroups kill killpg lchown linkat lstat lutimes mmap \
+#      memrchr mbrtowc mkdirat mkfifo \
+#      madvise mkfifoat mknod mknodat mktime mremap nice openat pathconf pause pipe2 plock poll \
+#    - posix_fallocate posix_fadvise posix_spawn posix_spawnp pread preadv preadv2 \
+#    - pthread_condattr_setclock pthread_init pthread_kill putenv pwrite pwritev pwritev2 \
+#    + posix_fallocate posix_fadvise posix_spawn posix_spawnp pread \
+#    + pthread_condattr_setclock pthread_init pthread_kill putenv pwrite \
+#      readlink readlinkat readv realpath renameat \
+#      sem_open sem_timedwait sem_getvalue sem_unlink sendfile setegid seteuid \
+#      setgid sethostname \
+#
+# start docker: termux-packages/scripts/run-docker.sh
+# build inside container: ./build-package.sh -a aarch64 python
+# copy outside container: mkdir /tmp/termux-packages && docker cp termux-package-builder:/home/builder/termux-packages/debs/python_3.8.2_arm.deb /tmp/termux-packages
+
+LOCAL_OVERRIDE_PATH = '/tmp/termux-packages'
+LOCAL_OVERRIDE_PKG = {'python': 'python_3.8.2_aarch64.deb'}
 
 def load_packages():
     pkg_deps = {}
@@ -58,18 +100,23 @@ def install_package(pkg_deps, pkg_filenames, pkg):
     if not os.path.exists('out'):
         os.mkdir('out')
 
-    if pkg not in pkg_filenames:
+    tmp_dir = tempfile.mkdtemp()
+
+    if pkg in LOCAL_OVERRIDE_PKG:
+        deb_name = LOCAL_OVERRIDE_PKG[pkg]
+        deb_path = os.path.join(LOCAL_OVERRIDE_PATH, deb_name)
+        print("Using local copy of package %s - %s - %s" % (pkg, tmp_dir, deb_name))
+    elif pkg in pkg_filenames:
+        url = BASE_URL + pkg_filenames[pkg]
+        print("Downloading %s - %s - %s" % (pkg, tmp_dir, url))
+        r = requests.get(url)
+        deb_name = 'out.deb'
+        deb_path = os.path.join(tmp_dir, deb_name)
+        open(deb_path, 'wb').write(r.content)
+    else:
         print("%s not found" % pkg)
         return ""
 
-    url = BASE_URL + pkg_filenames[pkg]
-    tmp_dir = tempfile.mkdtemp()
-
-    print("Downloading %s - %s - %s" % (pkg, tmp_dir, url))
-    r = requests.get(url)
-
-    deb_path = os.path.join(tmp_dir, 'out.deb')
-    open(deb_path, 'wb').write(r.content)
     subprocess.check_call(['ar', 'x', deb_path], cwd=tmp_dir)
     subprocess.check_call(['tar', '-C', './out', '-p', '-xf', os.path.join(tmp_dir, 'data.tar.xz')])
     if os.path.exists(os.path.join(tmp_dir, 'control.tar.gz')):
@@ -80,7 +127,7 @@ def install_package(pkg_deps, pkg_filenames, pkg):
     control = open(os.path.join(tmp_dir, 'control')).read()
     control += 'Status: install ok installed\n'
 
-    files = subprocess.check_output(['dpkg', '-c', 'out.deb'], cwd=tmp_dir)
+    files = subprocess.check_output(['dpkg', '-c', deb_path], cwd=tmp_dir)
 
     file_list = ""
     for f in files.split('\n'):
