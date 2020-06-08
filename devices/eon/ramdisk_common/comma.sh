2d8262a7833fb2f852b3a4b201f3901362492106
Jason Young <jyoung8607@gmail.com>
2020-06-08 11:04:43 -0400

    Bracket SSH key finalize with syncs to disk

--- devices/eon/ramdisk_common/comma.sh
+++ devices/eon/ramdisk_common/comma.sh
@@ -60,10 +60,12 @@ if [ ! -f /persist/comma/id_rsa.pub ]; then
 
   openssl genrsa -out /persist/comma/id_rsa.tmp 2048 &&
     openssl rsa -in /persist/comma/id_rsa.tmp -pubout -out /persist/comma/id_rsa.tmp.pub &&
+    sync &&
     mv /persist/comma/id_rsa.tmp /persist/comma/id_rsa &&
-    mv /persist/comma/id_rsa.tmp.pub /persist/comma/id_rsa.pub
-  chmod 755 /persist/comma/
-  chmod 744 /persist/comma/id_rsa
+    mv /persist/comma/id_rsa.tmp.pub /persist/comma/id_rsa.pub &&
+    chmod 755 /persist/comma/ &&
+    chmod 744 /persist/comma/id_rsa &&
+    sync
 fi
 
 rm -f /data/params/d/AthenadPid
