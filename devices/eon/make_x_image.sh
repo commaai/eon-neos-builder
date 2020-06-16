298391329f29fea849a591a02972f8ecb7c67295
Jason Young <jason@comma.ai>
2020-06-16 11:31:18 -0700

    Build NEOS 14 with matched kernel

--- devices/eon/make_x_image.sh
+++ devices/eon/make_x_image.sh
@@ -15,11 +15,12 @@ $TOOLS/extract_toolchains.sh
 mkdir -p $OUT
 
 if [ ! -d android_kernel_comma_msm8996 ]; then
-  git clone https://github.com/commaai/android_kernel_comma_msm8996.git --depth 1
+  git clone https://github.com/commaai/android_kernel_comma_msm8996.git --depth 50
 fi
 
 # Compile kernel
 cd android_kernel_comma_msm8996
+git checkout d2918fd9489da0eeda32ff7e465a28b0e0b94b5f
 git pull
 make comma_defconfig
 make -j$(nproc --all)
