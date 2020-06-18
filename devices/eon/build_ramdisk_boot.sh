449df4f8b80e0124306b4080a47c628d897753dd
Jason Young <jason@comma.ai>
2020-06-18 09:20:18 -0700

    Update NEOS RC

--- devices/eon/build_ramdisk_boot.sh
+++ devices/eon/build_ramdisk_boot.sh
@@ -21,7 +21,7 @@ pushd boot_ramdisk
   ln -s /data/data/com.termux/files/tmp tmp
   ln -s /data/data/com.termux/files/usr usr
   sudo cp -v "$DIR"/ramdisk_common/* .
-  echo "15" > VERSION
+  echo "15-RC2" > VERSION
   touch EON
 
   # repack ramdisk
