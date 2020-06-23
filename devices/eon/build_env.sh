8619593fdaffca4ca65785fd7bfe0eec0dd5b835
Jason Young <jason@comma.ai>
2020-06-23 12:55:46 -0700

    Add missing file from prior commit

---
+++ devices/eon/build_env.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# NEOS_VERSION sets the /VERSION for the new build. This value MUST be updated
+# for any change put into test/staging or production. For interim builds, use 
+# a value like "15-RC3" or similar, so that any device with a test build will
+# update to the final release version.
+
+export NEOS_BUILD_VERSION="14"
+
+# NEOS_BASE_FOR_USR sets the OTA image used for normal NEOS builds, where the
+# Termux /system base binaries are copied from the production shipping OTA
+# image instead of rebuilding from scratch. Ignored if CLEAN_USR=1.
+
+export NEOS_BASE_FOR_USR="https://raw.githubusercontent.com/commaai/eon-neos/d22cfd2123e13fded340e6290eea8204c73ce9a2/update.json"
+
+# NEOS_BASE_FOR_DASHCAM is used by build_dashcam_images.sh to set the base OTA
+# image on which to build a dashcam-cached system image. These images are used
+# in the Comma manufacturing process and also for PC desktop flashing. Before
+# and after NEOS versions must match.
+
+export NEOS_BASE_FOR_DASHCAM="https://raw.githubusercontent.com/commaai/openpilot/dashcam-staging/installer/updater/update.json"
