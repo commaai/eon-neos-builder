a39d9bf4c63afe49a70a74fba56a2345e94ef00c
builder <yuvch122@gmail.com>
2021-02-23 06:43:49 +0000

    Revert "fix flex problem"
    This reverts commit 0d4a59203fc91ede5b2b1d16a5a90eec01aa3131.

--- devices/eon/build_android.sh
+++ devices/eon/build_android.sh
@@ -29,6 +29,5 @@ cd $DIR/mindroid/system
 $TOOLS/repo init -u https://github.com/commaai/android.git -b "repeatable-build-mindroid"
 $TOOLS/repo sync -c -j$JOBS
 
-export LC_ALL=C
 export PATH=$PWD/bin:$PATH
 (source build/envsetup.sh && breakfast oneplus3 && make -j$JOBS)
