3fafa07e648f3f64f659399e37a6e3f6fa97ae74
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-03-01 15:05:19 -0800

    keep key for setup

--- devices/eon/ramdisk_common/comma.ssh.sh
+++ devices/eon/ramdisk_common/comma.ssh.sh
@@ -1,5 +1,19 @@
 #!/system/bin/sh
 
+if [[ ! -e /data/params/d/GithubSshKeys && ! -e /data/data/com.termux/files/continue.sh ]]; then
+  if [ ! -e /data/params/d ]; then
+    mkdir -p /data/params/d_tmp
+    ln -s /data/params/d_tmp /data/params/d
+  fi
+
+  cp /data/data/com.termux/files/home/setup_keys /data/params/d/GithubSshKeys
+
+elif [[ -e /data/params/d/GithubSshKeys && -e /data/data/com.termux/files/continue.sh ]]; then
+  if cmp -s /data/params/d/GithubSshKeys /data/data/com.termux/files/home/setup_keys; then
+    rm /data/params/d/GithubSshKeys
+  fi
+fi
+
 # init race conditions
 if [ "$(getprop persist.neos.ssh)" = "1" ]; then
   export HOME=/data/data/com.termux/files/home
