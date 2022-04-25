f5d32dcda93454adf13969139367d4a976f11a00
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2022-04-25 15:09:22 -0700

    NEOS 20 (#59)
    * NEOS 20
    
    * add pytest
    
    * Revert "add pytest"
    
    This reverts commit e9544deac222753b324dc450a3540cd3af46c2bf.
    
    * neos 19 base

--- devices/eon/build_env.sh
+++ devices/eon/build_env.sh
@@ -4,9 +4,9 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 export PATH="$DIR/bin:$PATH"
 
 # sets /VERSION for the build
-export NEOS_BUILD_VERSION="19.1"
+export NEOS_BUILD_VERSION="20"
 
 # NEOS_BASE_FOR_USR sets the OTA image used for normal NEOS builds, where the
 # Termux /system base binaries are copied from the production shipping OTA
 # image instead of rebuilding from scratch. Ignored if CLEAN_USR set.
-export NEOS_BASE_FOR_USR="https://raw.githubusercontent.com/commaai/openpilot/6a9514570cfeb2bf9b15929b2a6d3748ff2eb9a6/selfdrive/hardware/eon/neos.json"
+export NEOS_BASE_FOR_USR="https://raw.githubusercontent.com/commaai/openpilot/62e0bd5f0d40b0ce1be171332d50de618ef5d51c/selfdrive/hardware/eon/neos.json"
